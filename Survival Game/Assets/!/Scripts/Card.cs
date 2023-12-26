23011384cf3b67d61c174b60fdc416fcef038de1
Hades <shamardakdmutro2004@gmail.com>
2023-12-26 14:25:15 +0200

    add randomness to card instantiating
    add attack speed card

--- Survival Game/Assets/!/Scripts/Card.cs
+++ Survival Game/Assets/!/Scripts/Card.cs
@@ -1,17 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+using UnityEngine.UI;
 
 public class Card : MonoBehaviour {
 
     [SerializeField] private StatModifier _statModifier;
     [SerializeField] private StatType _statType;
+    private LevelUpPanel _levelUpUI;
+    private Button _button;
     private PlayerStats _playerStats;
 
     private void Start()
     {
         _playerStats = GameObject.Find("Player").GetComponent<PlayerStats>();
+        _levelUpUI = GameObject.Find("LevelUpUI").GetComponent<LevelUpPanel>();
+
+        _button = GetComponent<Button>();
+        _button.onClick.AddListener(_levelUpUI.ContinueGame);
     }
 
     public void OnClickModify()
--- Survival Game/Assets/!/Scripts/LevelUpPanel.cs
+++ Survival Game/Assets/!/Scripts/LevelUpPanel.cs
@@ -1,18 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class LevelUpPanel : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private GameObject _levelUpCanvas;
+    [SerializeField] private GameObject _panel;
+    [SerializeField] private int _maxCards;
+    [SerializeField] private Vector2 _firstElementPosition;
+    [SerializeField] private int _distanceBetweenCards;
+    [SerializeField] private List<GameObject> _cards;
+    private List<GameObject> _spawnedObj = new List<GameObject>();
+    private Level _playerLevel;
+
     void Start()
     {
-        
+        _playerLevel = GameObject.Find("Player").GetComponent<Level>();        
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        ShowPanel();
+    }
+
+    private void ShowPanel()
+    {
+        if (_playerLevel.LevelUp())
+        {
+            _levelUpCanvas.SetActive(true);
+
+            List<GameObject> tempList = new List<GameObject>(_cards);
+
+            //Instantiate random object from temp list then remove from list
+            int count = tempList.Count;
+            int elements = _maxCards;
+            Vector2 position = _firstElementPosition;
+
+            for (int i = count; i > 0; i--)
+            {
+                if(elements == 0)
+                {
+                    break;
+                }
+                int randomElement = Random.Range(0, i);
+                GameObject card = Instantiate(tempList[randomElement], position, _panel.transform.rotation, _panel.transform);
+                position.x += _distanceBetweenCards;
+
+                tempList.RemoveAt(randomElement);
+                _spawnedObj.Add(card);
+
+                elements--;
+                count--;
+            }
+
+            Time.timeScale = 0;
+        }
+    }
+
+    public void ContinueGame()
+    {
+        _levelUpCanvas.SetActive(false);
+
+        //delete all instantiated objects
+        for (int i = _spawnedObj.Count - 1; i >= 0; i--)
+        {
+            Destroy(_spawnedObj[i]);
+            _spawnedObj.RemoveAt(i);
+        }
+        Time.timeScale = 1;
     }
 }
